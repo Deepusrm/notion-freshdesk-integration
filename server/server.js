d8ae10c8752167d65e2238d50b8c5408bf6a9832
Deepika Sriram <deepika.sriram1@freshworks.com>
2023-12-26 17:22:32 +0530

    installed dotenv

--- server/server.js
+++ server/server.js
@@ -1,34 +1,19 @@
+require('dotenv').config();
+
 exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
   onConversationCreateHandler: async function(){
-    // const noteData = args['data']['conversation'];
-    // try{
-    //   if(noteData.private==true){
-    //     await $request.invokeTemplate("onCreatingPrivateNote",{
-    //       context:{},
-    //       body:JSON.stringify(
-    //         {
-    //           "parent": {"database_id": "<%=iparam.notion_page%>"},
-    //           "properties": {
-    //               "title": {"title": [{"text": {"content": "My Notion Page"}}]},
-    //               "body": {"rich_text": [{"text": {"content": "This is the content of my page."}}]},
-    //           },
-    //         })
-    //       })
-    //     console.log("notion page created successfully!!");
-    //   }
-    // }catch(error){
-    //   console.log(error);
-    // }
-
-    // console.log(noteData.private);
-
     try{
-      const pages = await $request.invokeTemplate("onGettingPages",{});
-      console.log(pages.response);
+      const conversationData = args["data"];
+      console.log(conversationData);
+      if(conversationData.private==true){
+        $request.invokeTemplate("onCreatingPrivateNote",{})
+      }
     }catch(error){
-      console.log(error);
+      console.log("Failed due to "+error);
     }
+
+    const databaseId = process.env;
   }
-};
+}
