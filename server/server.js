1baf87e4887d573d4d825bf76542509d77a5daef
Deepika Sriram <deepika.sriram1@freshworks.com>
2023-12-26 21:42:49 +0530

    creating pages on notion for private note in conversation in freshdesk

--- server/server.js
+++ server/server.js
@@ -1,19 +1,54 @@
-require('dotenv').config();
-
 exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
-  onConversationCreateHandler: async function(){
-    try{
-      const conversationData = args["data"];
-      console.log(conversationData);
-      if(conversationData.private==true){
-        $request.invokeTemplate("onCreatingPrivateNote",{})
+  onConversationCreateHandler: async function(args){
+    const noteData = args["data"]["conversation"];
+    if(noteData.private==true){
+      try{
+        await $request.invokeTemplate("onCreatingPrivateNote",{
+          context:{},
+          body:JSON.stringify(
+            {
+              parent:{
+                type:"database_id",
+                database_id:"<%=iparam.notion_database%>"
+              },
+              properties:{
+                Name:{
+                  title:[
+                    {
+                      type:"text",
+                      text:{
+                        content: "TICKET #"+noteData["ticket_id"]
+                      }
+                    }
+                  ]
+                }
+              },
+              children:[
+                {
+                  object:"block",
+                  type:"paragraph",
+                  paragraph:{
+                    rich_text:[
+                      {
+                        text:{
+                          content:noteData["body_text"]
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          )
+        })
+        console.log("Notes added successfully to the notion :) ");
+      }catch(error){
+        console.log(error);
       }
-    }catch(error){
-      console.log("Failed due to "+error);
+    }else{
+      console.log("Note is not private");
     }
-
-    const databaseId = process.env;
   }
 }
