382bc18ac15419ec6369902cd3298163d9931707
Deepika Sriram <deepika.sriram1@freshworks.com>
2023-12-26 15:08:29 +0530

    made api calls from notion

--- server/server.js
+++ server/server.js
@@ -2,26 +2,33 @@ exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
   onConversationCreateHandler: async function(){
-    const noteData = args['data']['conversation'];
+    // const noteData = args['data']['conversation'];
+    // try{
+    //   if(noteData.private==true){
+    //     await $request.invokeTemplate("onCreatingPrivateNote",{
+    //       context:{},
+    //       body:JSON.stringify(
+    //         {
+    //           "parent": {"database_id": "<%=iparam.notion_page%>"},
+    //           "properties": {
+    //               "title": {"title": [{"text": {"content": "My Notion Page"}}]},
+    //               "body": {"rich_text": [{"text": {"content": "This is the content of my page."}}]},
+    //           },
+    //         })
+    //       })
+    //     console.log("notion page created successfully!!");
+    //   }
+    // }catch(error){
+    //   console.log(error);
+    // }
+
+    // console.log(noteData.private);
+
     try{
-      if(noteData.private==true){
-        await $request.invokeTemplate("onCreatingPrivateNote",{
-          context:{},
-          body:JSON.stringify(
-            {
-              "parent": {"database_id": "<%=iparam.notion_page%>"},
-              "properties": {
-                  "title": {"title": [{"text": {"content": "My Notion Page"}}]},
-                  "body": {"rich_text": [{"text": {"content": "This is the content of my page."}}]},
-              },
-            })
-          })
-        console.log("notion page created successfully!!");
-      }
+      const pages = await $request.invokeTemplate("onGettingPages",{});
+      console.log(pages.response);
     }catch(error){
       console.log(error);
     }
-
-    console.log(noteData.private);
   }
 };
