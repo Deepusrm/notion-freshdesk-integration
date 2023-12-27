8137cc27e65a5c67186e43ee62e4ca5fc368cd5a
Deepika Sriram <deepika.sriram1@freshworks.com>
2023-12-27 16:17:38 +0530

    changed the layout of paragraph into to-do list layout

--- server/server.js
+++ server/server.js
@@ -1,53 +1,77 @@
 exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
-  onConversationCreateHandler: async function(args){
+  onConversationCreateHandler: async function (args) {
     const noteData = args["data"]["conversation"];
-    if(noteData.private==true){
-      try{
-        await $request.invokeTemplate("onCreatingPrivateNote",{
-          context:{},
-          body:JSON.stringify(
-            {
-              parent:{
-                type:"database_id",
-                database_id:"<%=iparam.notion_database%>"
-              },
-              properties:{
-                Name:{
-                  title:[
-                    {
-                      type:"text",
-                      text:{
-                        content: "TICKET #"+noteData["ticket_id"]
-                      }
-                    }
-                  ]
+
+    // checking whether the mode of the note is private or not
+    if (noteData.private == true) {
+      // for a to-do list, splitting whole string into an array
+      const todoList = noteData["body_text"].split("\n");
+
+      // creating body JSON to send the details for POST request
+      const bodyJSON = {
+        parent: {
+          type: "database_id",
+          database_id: "<%=iparam.notion_database%>"
+        },
+        properties: {
+          Name: {
+            title: [
+              {
+                type: "text",
+                text: {
+                  content: "TICKET #" + noteData["ticket_id"]
                 }
-              },
-              children:[
+              }
+            ]
+          }
+        },
+        children: [
+          {
+            object: "block",
+            type: "heading_2",
+            heading_2: {
+              rich_text: [
                 {
-                  object:"block",
-                  type:"paragraph",
-                  paragraph:{
-                    rich_text:[
-                      {
-                        text:{
-                          content:noteData["body_text"]
-                        }
-                      }
-                    ]
+                  text: {
+                    content: todoList[0]
                   }
                 }
               ]
             }
-          )
+          }
+        ]
+      }
+
+      // for n number of to-do list, iterating, creating a new block object and pushing into children array
+      for (let i = 1; i < todoList.length; i++) {
+        bodyJSON["children"].push({
+          object: "block",
+          type: "to_do",
+          to_do: {
+            rich_text: [
+              {
+                text: {
+                  content: todoList[i]
+                }
+              }
+            ],
+            checked: false,
+            color: "blue"
+          }
+        })
+      }
+      try {
+        await $request.invokeTemplate("onCreatingPrivateNote", {
+          context: {},
+          body: JSON.stringify(bodyJSON)
         })
         console.log("Notes added successfully to the notion :) ");
-      }catch(error){
+      } catch (error) {
         console.log(error);
       }
-    }else{
+    } else {
       console.log("Note is not private");
     }
   }
