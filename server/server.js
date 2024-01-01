60ed02a4373fd7f99314d58d15736c983fb73a5d
Deepika Sriram <deepika.sriram1@freshworks.com>
2024-01-01 17:24:24 +0530

    implementing the feature of mapping to same page

--- server/server.js
+++ server/server.js
@@ -1,91 +1,106 @@
 exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
+  
+
+
   onConversationCreateHandler: async function (args) {
+    const noteData = args.data.conversation;
 
-    const noteData = args["data"]["conversation"];
-
-    // checking whether the mode of the note is private or not
     if (noteData.private == true) {
+      const ticketKey = `ticket-${noteData["ticket_id"]}`;
+      try {
+        const dbstore = $db.set(ticketKey, { "notionPageId": ""}, {setIf: "not_exist" });
+        console.log(dbstore);
 
-      // creating body JSON to send the details for POST request
-      const bodyJSON = {
-        parent: {
-          type: "database_id",
-          database_id: "<%=iparam.notion_database%>"
-        },
-        properties: {
-          Name: {
-            title: [
-              {
-                type: "text",
-                text: {
-                  content: "TICKET #" + noteData["ticket_id"]
-                }
-              }
-            ]
-          }
-        },
-        children: [
-          
-        ]
-      }
-
-      //console.log(noteData["body_text"]);
-      // giving different layout for a list and a paragraph 
-      if(noteData["body"].includes("<ul>")==true || noteData["body"].includes("<ol>")==true || noteData["body"].includes("</li>")==true){
-        const todoList = noteData["body_text"].split("  ");
-        //console.log(JSON.stringify(todoList));
-        for (let i = 0; i < todoList.length; i++) {
-          bodyJSON["children"].push({
-            object: "block",
-            type: "bulleted_list_item",
-            bulleted_list_item: {
-              rich_text: [
+        const bodyJSON = {
+          parent: {
+            type: "database_id",
+            database_id: "<%=iparam.notion_database%>"
+          },
+          properties: {
+            Name: {
+              title: [
                 {
-                  type:"text",
+                  type: "text",
                   text: {
-                    content: todoList[i]
+                    content: "TICKET #" + noteData["ticket_id"]
                   }
                 }
-              ],
+              ]
             }
-          })
+          },
+          children: []
         }
-      }else{
-        bodyJSON["children"].push({
-          object:"block",
-          type:"paragraph",
-          paragraph:{
-            rich_text: [
-              {
-                type:"text",
-                text: {
-                  content: noteData["body_text"]
-                }
-              }
-            ]
-          }
-        })
-      }
-      // for n number of to-do list, iterating, creating a new block object and pushing into children array
-      try {
-        const responseData = await $request.invokeTemplate("onCreatingPrivateNote", {
-          context: {},
-          body: JSON.stringify(bodyJSON)
-        })
-        const responseJSON = JSON.parse(responseData.response);
 
-        const ticketKey = `ticket-${noteData["ticket_id"]}`;
-        const dbstoreResponse = await $db.set(ticketKey,{"notionPageId":responseJSON.id});
-        console.log(dbstoreResponse);
+        appendBlock(bodyJSON, noteData["body"], noteData["body_text"]);
 
-        
-      } catch (error) {
-        console.log(error);
+        try {
+          const responseData = await $request.invokeTemplate("onCreatingPrivateNote", {
+            context: {},
+            body: JSON.stringify(bodyJSON)
+          })
+          const responseJSON = JSON.parse(responseData.response);
+          $db.update(ticketKey, "set", { "notionPageId": responseJSON.id },{setIf:"exist"})
+        } catch (error1) {
+          console.log("Error 1:"+error1);
+        }
+
+      } catch (error2) {
+        // console.log("Error 2"+error2);
+        const notion_page_id = $db.get(ticketKey).notionPageId;
+        // console.log("Page id "+notion_page_id)
+        const blockJSON = {
+          children:[]
+        }
+        appendBlock(blockJSON,noteData["body"],noteData["body_text"]);
+        try{
+          await $request.invokeTemplate("onAppendingToExistingNote",{
+            context:{page_id:notion_page_id},
+            body:JSON.stringify(blockJSON)
+          });
+          // console.log(responseData);
+        }catch(error3){
+          console.log("Error 3"+error3);
+        }
       }
-    } else {
-      console.log("Note is not private");
     }
+  }
+}
+
+function appendBlock(data, body, bodyText) {
+  if (body.includes("<ol>") == true || body.includes("<ul>") || body.includes("</li>") == true) {
+    const todoList = bodyText.split("  ");
+    for (let i = 0; i < todoList.length; i++) {
+      data["children"].push({
+        object: "block",
+        type: "bulleted_list_item",
+        bulleted_list_item: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: todoList[i]
+              }
+            }
+          ]
+        }
+      })
+    }
+  } else {
+    data["children"].push({
+      object: "block",
+      type: "paragraph",
+      paragraph: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: bodyText
+            }
+          }
+        ]
+      }
+    })
   }
 }
