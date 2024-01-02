ad5cf6025b5c98dce2f28c0fc5483b31602245cc
Deepika Sriram <deepika.sriram1@freshworks.com>
2024-01-02 16:45:59 +0530

    removed a request which is unused

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ exports = {
     if (noteData.private == true) {
       const ticketKey = `ticket-${noteData["ticket_id"]}`;
       try {
+        // setting the notion page id as null to update later.
         await $db.set(ticketKey, { "notionPageId": "" }, { setIf: "not_exist" });
         const bodyJSON = {
           parent: {
@@ -26,6 +27,7 @@ exports = {
           children: []
         }
 
+        // calling the function to append block based on the content
         appendBlock(bodyJSON, noteData["body"], noteData["body_text"]);
 
         try {
@@ -34,18 +36,23 @@ exports = {
             body: JSON.stringify(bodyJSON)
           })
           const responseJSON = JSON.parse(responseData.response);
+
+          // updating the notion page id from null to the actual value
           $db.update(ticketKey, "set", { "notionPageId": responseJSON.id }, { setIf: "exist" })
         } catch (error1) {
-          console.log("Error 1:" + error1);
+          throw new Error(error1);
         }
 
-      } catch (error2) {
+      } catch (error2) { // this code block is to the case if the notion page for that particular ticket has already been created
         const notion_page_id = await $db.get(ticketKey);
 
         const blockJSON = {
           children: []
         }
+
+        // calling the function to append block based on the content
         appendBlock(blockJSON, noteData["body"], noteData["body_text"]);
+
         try {
           await $request.invokeTemplate("onAppendingToExistingNote", {
             context: { page_id: notion_page_id["notionPageId"] },
@@ -53,7 +60,7 @@ exports = {
           });
           console.log("note added successfully");
         } catch (error3) {
-          console.log(error3);
+          throw new Error(error3);
         }
       }
     }
@@ -61,7 +68,7 @@ exports = {
 }
 
 function appendBlock(data, body, bodyText) {
-  if (body.includes("<ol>") == true || body.includes("<ul>") || body.includes("</li>") == true) {
+  if (body.includes("<ol>") == true || body.includes("<ul>") || body.includes("</li>") == true) { // for list type of content
     const todoList = bodyText.split("  ");
     for (let i = 0; i < todoList.length; i++) {
       data["children"].push({
@@ -79,7 +86,7 @@ function appendBlock(data, body, bodyText) {
         }
       })
     }
-  } else {
+  } else { // for paragraph type of content
     data["children"].push({
       object: "block",
       type: "paragraph",
@@ -95,7 +102,8 @@ function appendBlock(data, body, bodyText) {
       }
     })
   }
-  data["children"].push({
+  // to add a divider between separate notes of the same ticket.
+  data["children"].push({ 
     object:"block",
     type:"divider",
     divider:{}
