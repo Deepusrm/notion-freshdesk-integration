1b7fd1f133cfc063128dd7b39f851f2f3e8611d7
Deepika Sriram <deepika.sriram1@freshworks.com>
2024-01-04 12:43:37 +0530

    resolved the bug in changing the layout of the content in notion

--- server/functions.js
+++ server/functions.js
@@ -1,6 +1,27 @@
-exports.appendBlock = function appendBlock(data, body, bodyText) {
+exports.appendBlock = async function appendBlock(data, body, bodyText) {
     const list = bodyText.split("  ");
-    if (body.includes("<ol>") == true || body.includes("<ul>") == true || body.includes("</li>") == true) { // for list type of content
+    var res = getConditionResult(body);
+    console.log(res);
+    if (res == true && body.includes("to do")==true) { // for list type of content
+        list.forEach(element => {
+            data["children"].push({
+                object: "block",
+                type: "to_do",
+                to_do: {
+                    rich_text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: element
+                            }
+                        }
+                    ],
+                    checked:false,
+                    color:"default"
+                }
+            })
+        });
+    }else if(res==true){
         list.forEach(element => {
             data["children"].push({
                 object: "block",
@@ -39,4 +60,12 @@ exports.appendBlock = function appendBlock(data, body, bodyText) {
         type: "divider",
         divider: {}
     })
+}
+
+function getConditionResult(text){
+    if(text.includes("<ol>") == true || text.includes("<ul>") == true || text.includes("</li>") == true){
+        return true;
+    }else{
+        return false;
+    }
 }
