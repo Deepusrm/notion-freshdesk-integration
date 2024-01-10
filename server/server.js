02239c6ec7fe9ac8d3cb445b9d75860c7f323072
Deepika Sriram <deepika.sriram1@freshworks.com>
2024-01-10 17:25:30 +0530

    added methods to reduce the number of lines

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-var utilFunctions = require('./utilities');
+// import * as utils from './utilities'; 
+const utils = require('./utilities');
 exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
@@ -7,50 +8,17 @@ exports = {
 
     if (noteData.private == true) {
       const ticketKey = `ticket-${noteData["ticket_id"]}`;
-      var createdAt = utilFunctions.returnReadableDate(noteData["created_at"]);
-      var modifiedAt = utilFunctions.returnReadableDate(noteData["updated_at"]);
       try {
         // setting the notion page id as null to update later.
         await $db.set(ticketKey, { "notionPageId": "" }, { setIf: "not_exist" });
-        const bodyJSON = {
-          parent: {
-            type: "database_id",
-            database_id: "<%=iparam.notion_database%>"
-          },
-          properties: {
-            Name: {
-              title: [{
-                type: "text",
-                text: {
-                  content: "TICKET #" + noteData["ticket_id"]
-                }
-              }]
-            }
-          },
-          children: [
-            {
-              object: "block",
-              type: "paragraph",
-              paragraph: {
-                rich_text: [
-                  {
-                    type: "text",
-                    text: {
-                      content: `Created at ${createdAt}, modified at ${modifiedAt}`
-                    },
-                    annotations: {
-                      italic: true,
-                      color: "gray"
-                    }
-                  }
-                ]
-              }
-            }
-          ]
-        }
 
+        const parentJSON = (await utils.defaultParentBlock(args));
+        const childJSON = (await utils.defaultChildBlock(args));
+        const bodyJSON = {...parentJSON,...childJSON}
+
+        console.log(bodyJSON);
         // calling the function to append block based on the content
-        utilFunctions.appendBlock(bodyJSON, noteData["body"], noteData["body_text"]);
+        utils.appendBlock(bodyJSON, noteData["body"], noteData["body_text"]);
 
         const responseData = await $request.invokeTemplate("onCreatingPrivateNote", {
           context: {},
@@ -65,34 +33,14 @@ exports = {
 
       } catch (error) { // this code block is to the case if the notion page for that particular ticket has already been created
         console.log(error);
-        if (error["message"]==="The setIf conditional request failed") {
+        if (error.message === "The setIf conditional request failed") {
+          // newNoteOnExistingPageHandler();
           const notion_page_id = await $db.get(ticketKey);
-
-          const blockJSON = {
-            children: [
-              {
-                object: "block",
-                type: "paragraph",
-                paragraph: {
-                  rich_text: [
-                    {
-                      type: "text",
-                      text: {
-                        content: `Created at ${createdAt}, modified at ${modifiedAt}`
-                      },
-                      annotations: {
-                        italic: true,
-                        color: "gray"
-                      }
-                    }
-                  ]
-                }
-              }
-            ]
-          }
+          const block = utils.defaultChildBlock(args);
+          const blockJSON = {...block}
 
           // calling the function to append block based on the content
-          utilFunctions.appendBlock(blockJSON, noteData["body"], noteData["body_text"]);
+          utils.appendBlock(blockJSON, noteData["body"], noteData["body_text"]);
           await $request.invokeTemplate("onAppendingToExistingNote", {
             context: { page_id: notion_page_id["notionPageId"] },
             body: JSON.stringify(blockJSON)
@@ -103,9 +51,6 @@ exports = {
         }
       }
     }
-  }
-
-  // onConversationUpdateHandler: async function(args){
-
-  // }
+  },
+  
 }
--- server/utilities.js
+++ server/utilities.js
@@ -1,9 +1,85 @@
+
+exports.defaultParentBlock = async function (payload)  {
+    const noteData = payload.data.conversation;
+
+    const parentJSON = {
+        parent: {
+            type: "database_id",
+            database_id: "<%=iparam.notion_database%>"
+        },
+        properties: {
+            Name: {
+                title: [{
+                    type: "text",
+                    text: {
+                        content: "TICKET #" + noteData["ticket_id"]
+                    }
+                }]
+            }
+        }
+    }
+
+    return parentJSON
+}
+
+function returnReadableDate(timestamp)  {
+    const date = new Date(timestamp);
+    const options = {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+    }
+    const readableDate = date.toLocaleString('en-US', options)
+    console.log(readableDate);
+    return readableDate;
+}
+
+
+exports.defaultChildBlock = async function(payload)  {
+    const noteData = payload.data.conversation;
+    
+    let createdAt = returnReadableDate(noteData["created_at"]);
+    let modifiedAt = returnReadableDate(noteData["updated_at"]);
+
+    const childJSON = {
+        children: [
+            {
+                object: "block",
+                type: "paragraph",
+                paragraph: {
+                    rich_text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: `Created at ${createdAt}, modified at ${modifiedAt}`
+                            },
+                            annotations: {
+                                italic: true,
+                                color: "gray"
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+
+    return childJSON
+}
+
 exports.appendBlock = async function appendBlock(data, body, bodyText) {
+    const result = { ...data };
     const list = bodyText.split("  ");
-    var res = getConditionResult(body);
-    if (res == true && body.includes("to do") == true) { // for list type of content
+    const isList = getIfList(body);
+    const isTodoList = getIfList(body) && body.toLowerCase().includes("todo");
+    
+    if (isList && isTodoList) { // for list type of content
         list.forEach(element => {
-            data["children"].push({
+            result["children"].push({
                 object: "block",
                 type: "to_do",
                 to_do: {
@@ -20,9 +96,10 @@ exports.appendBlock = async function appendBlock(data, body, bodyText) {
                 }
             })
         });
-    } else if (res == true) {
+        return
+    } else if (isList) {
         list.forEach(element => {
-            data["children"].push({
+            result["children"].push({
                 object: "block",
                 type: "bulleted_list_item",
                 bulleted_list_item: {
@@ -37,8 +114,9 @@ exports.appendBlock = async function appendBlock(data, body, bodyText) {
                 }
             })
         });
+        return
     } else { // for paragraph type of content
-        data["children"].push({
+        result["children"].push({
             object: "block",
             type: "paragraph",
             paragraph: {
@@ -51,36 +129,22 @@ exports.appendBlock = async function appendBlock(data, body, bodyText) {
                     }
                 ]
             }
-        })
+        });
+
+        // return result;
     }
     // to add a divider between separate notes of the same ticket.
-    data["children"].push({
+    result["children"].push({
         object: "block",
         type: "divider",
         divider: {}
     })
 }
 
-function getConditionResult(text) {
+function getIfList(text) {
     if (text.includes("<ol>") == true || text.includes("<ul>") == true || text.includes("</li>") == true) {
         return true;
     } else {
         return false;
     }
-}
-
-exports.returnReadableDate = (timestamp) => {
-    const date = new Date(timestamp);
-    const options = {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: true,
-    }
-    const readableDate = date.toLocaleString('en-US', options)
-    console.log(readableDate);
-    return readableDate;
 }
